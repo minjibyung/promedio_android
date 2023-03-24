b986e5c3f3d90d3e0441eb0f86fed196d58f7065
minjibyung <minjibyung@gmail.com>
2023-03-24 13:18:15 -0500

    se agregan cambios

--- app/src/main/java/com/example/promedio_notas/ActivityMensage.kt
+++ app/src/main/java/com/example/promedio_notas/ActivityMensage.kt
@@ -2,6 +2,7 @@ package com.example.promedio_notas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class ActivityMensage : AppCompatActivity() {
@@ -11,15 +12,20 @@ class ActivityMensage : AppCompatActivity() {
 
         val campoMensaje=findViewById<TextView>(R.id.txtMensaje)
 
-        var miBundle: Bundle? =this.intent.extras
+        val miBundle: Bundle? =this.intent.extras
 
         if (miBundle != null) {
-            campoMensaje.text="Hola ${miBundle.getString("Nombre")} Bienvenid@" +
+            campoMensaje.text = "Hola ${miBundle.getString("Nombre")} Bienvenid@" +
                     "La materia a validar es ${miBundle.getString("Materia")}" +
                     "Las notas a validar son: Nota 1 ${miBundle.getString("Nota1")}" +
-                    "Nota 2 ${miBundle.getString("Nota2")}\" +"
-
-
+                    "Nota 2 ${miBundle.getString("Nota2")}\"" +
+                    "Nota 3 ${miBundle.getString("Nota3")}"
         }
+val botonSalir:Button=findViewById(R.id.btnSalir)
+        botonSalir.setOnClickListener { onClick() }
+        }
+
+    private fun onClick() {
+        finish()
     }
 }
--- app/src/main/java/com/example/promedio_notas/MainActivity.kt
+++ app/src/main/java/com/example/promedio_notas/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.promedio_notas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,41 +8,49 @@ import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         val botonCalcular:Button= findViewById(R.id.btnCalcular)
         botonCalcular.setOnClickListener{Calcular()}
     }
 
     private fun Calcular() {
+        val campoTexto:EditText?=findViewById(R.id.idNombre)
+        val campoMateria1:EditText?=findViewById(R.id.idMateria)
+        val campoNota1:EditText?=findViewById(R.id.idNota1)
+        val campoNota2:EditText?=findViewById(R.id.idNota2)
+        val campoNota3:EditText?=findViewById(R.id.idNota3)
 
-        //val intent =Intent(this,ActivityMensage::class.java)
-        //val miBundle:Bundle= Bundle()
-       // miBundle.putString("Nombre","Materia","Nota1","Nota2")
+        val name = campoTexto?.text.toString()
+        val subject = campoMateria1?.text.toString()
 
-        val campoNombre:EditText=findViewById(R.id.idNombre)
-        val campoMateria:EditText=findViewById(R.id.idMateria)
-        val campoNota1:EditText=findViewById(R.id.idNota1)
-        val campoNota2:EditText=findViewById(R.id.idNota2)
-        val campoNota3:EditText=findViewById(R.id.idNota3)
 
-        val nombre:String=campoNombre.text.toString()
-        val materia:String=campoMateria.text.toString()
-        val nota1:Double=campoNota1.text.toString().toDouble()
-        val nota2:Double=campoNota2.text.toString().toDouble()
-        val nota3:Double=campoNota3.text.toString().toDouble()
 
-        var prom:Double=(nota1+nota2+nota3)/3
+        val intent = Intent(this,ActivityMensage::class.java)
+        val miBundle:Bundle= Bundle()
+        miBundle.putString("Nombre",name)
+        miBundle.putString("Materia",subject)
+        intent.putExtras(miBundle)
+        startActivity(intent)
+
+        val nota1:Double= campoNota1?.text.toString().toDouble()
+        val nota2:Double= campoNota2?.text.toString().toDouble()
+        val nota3:Double= campoNota3?.text.toString().toDouble()
+
+        val prom:Double=(nota1+nota2+nota3)/3
         println("El promedio de notas es de: "+prom)
 
         if (prom>=3.5){
-            println("EL ESTUDIANTE $nombre GANO LA MATERIA DE $materia CON UNA NOTA DE: "+prom)
+            println("EL ESTUDIANTE $name GANO LA MATERIA DE $subject CON UNA NOTA DE: "+prom)
         }else {
-            println("EL ESTUDIANTE $nombre PERDIO LA MATERIA DE $materia CON UNA NOTA DE: " + prom)
+            println("EL ESTUDIANTE $name PERDIO LA MATERIA DE $subject CON UNA NOTA DE: " + prom)
         }
-    Toast.makeText(this,"Hello $nombre",Toast.LENGTH_LONG).show()
+    Toast.makeText(this,"Hello $name",Toast.LENGTH_LONG).show()
     }
 
     }
