fe14703f8670ef9ede4bf4fb31f0db1d3e1c2675
jheison rua <ruateo555@gmail.com>
2023-03-24 20:41:16 -0500

    cambioos

--- app/src/main/java/com/example/promedio_notas/ActivityMensage.kt
+++ app/src/main/java/com/example/promedio_notas/ActivityMensage.kt
@@ -1,6 +1,7 @@
 package com.example.promedio_notas
 
 import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,29 +13,30 @@ class ActivityMensage : AppCompatActivity() {
         setContentView(R.layout.activity_mensage)
 
         val campoMensaje = findViewById<TextView>(R.id.txtMensaje)
-        val mensajeAR = findViewById<TextView>(R.id.idmessage)
 
-        val miBundle: Bundle? = this.intent.extras
+        var miBundle: Bundle? = this.intent.extras
 
-        if (miBundle != null) {
-            campoMensaje.text = "Hola ${miBundle.getString("Nombre")} Bienvenid@\n" +
-                    "La materia a validar es: ${miBundle.getString("Materia")}\n" +
-                    "Las notas a validar son: Nota 1: ${miBundle.getDouble("Nota1")}\n" +
-                    "Nota 2: ${miBundle.getDouble("Nota2")}\n" +
-                    "Nota 3: ${miBundle.getDouble("Nota3")}\n" +
-                    "Y su promedio de notas es de: ${miBundle.getDouble("Promedio")}\n"
+
+        if(miBundle != null){
+            campoMensaje.text="Alumno: ${miBundle.getString("Nombre")} \nMateria: ${miBundle.getString("Materia")} \n\nSus notas: \nNota 1:  ${miBundle.getDouble("Nota1")} \nNota 2: ${miBundle.getDouble("Nota2")} \nNota 3:  ${miBundle.getDouble("Nota3")} \n\nPromedio: ${miBundle.getString("Promedio")} \n\nEl estado es: "
+
         }
-        if (miBundle?.getString("prom")!!>=3.5.toString()) {
-            mensajeAR.text="${miBundle.getString("message1")}\n"
-            mensajeAR.setTextColor(Color.parseColor("#ffffff"))
+        //Pintar texto
+        val mensajeAR=findViewById<TextView>(R.id.idmessage)
+        if(miBundle?.getString("Promedio")!! >= 3.5.toString()){
+            mensajeAR.text=" ${miBundle?.getString("message")}"
+            mensajeAR.setTextColor(Color.parseColor("#4CAF50"));
+            mensajeAR.setTypeface(null, Typeface.BOLD);
         }else{
-            mensajeAR.text="${miBundle.getString("message1")}\n"
-            mensajeAR.setTextColor(Color.LTGRAY)
-        }
+            mensajeAR.text=" ${miBundle?.getString("message")}"
+            mensajeAR.setTypeface(null, Typeface.BOLD);
+            mensajeAR.setTextColor(Color.parseColor("#C62828"));
 
-        val botonSalir:Button=findViewById(R.id.btnSalir)
-        botonSalir.setOnClickListener { onClick() }
         }
+        val botonSalir:Button = findViewById(R.id.btnSalir)
+        botonSalir.setOnClickListener{onClick()}
+
+    }
 
     private fun onClick() {
         finish()
--- app/src/main/java/com/example/promedio_notas/MainActivity.kt
+++ app/src/main/java/com/example/promedio_notas/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,49 +14,47 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val botonCalcular:Button= findViewById(R.id.btnCalcular)
+        val botonCalcular:Button = findViewById(R.id.btnCalcular)
         botonCalcular.setOnClickListener{Calcular()}
+
     }
 
     private fun Calcular() {
-        val campoTexto:EditText?=findViewById(R.id.idNombre)
-        val campoMateria1:EditText?=findViewById(R.id.idMateria)
-        val campoNota1:EditText?=findViewById(R.id.idNota1)
-        val campoNota2:EditText?=findViewById(R.id.idNota2)
-        val campoNota3:EditText?=findViewById(R.id.idNota3)
+        val campoNota1: EditText = findViewById(R.id.idNota1)
+        val campoNota2: EditText = findViewById(R.id.idNota2)
+        val campoNota3: EditText = findViewById(R.id.idNota3)
+        val campoTexto: EditText = findViewById(R.id.idNombre)
+        val campoMateria1: EditText = findViewById(R.id.idMateria)
 
+        val nota1:Double = campoNota1.text.toString().toDouble()
+        val nota2:Double = campoNota2.text.toString().toDouble()
+        val nota3:Double = campoNota3.text.toString().toDouble()
+        val name:String = campoTexto.text.toString()
+        val subject:String = campoMateria1.text.toString()
 
-        val name = campoTexto?.text.toString()
-        val subject = campoMateria1?.text.toString()
-        val nota1:Double= campoNota1?.text.toString().toDouble()
-        val nota2:Double= campoNota2?.text.toString().toDouble()
-        val nota3:Double= campoNota3?.text.toString().toDouble()
-
-        val intent = Intent(this,ActivityMensage::class.java)
-        val miBundle:Bundle= Bundle()
-        val prom:Double=(nota1+nota2+nota3)/3
+        val prom: Double = (nota1 + nota2 + nota3) / 3.0
+        val formattedAverage = String.format("%.2f", prom)
 
         var message: String? = null
 
-
-
-        if (prom>=3.5){
-            message = "El estudiante aprobo"
-        }else {
-            message = "El estudiante no aprobo"
+        if (prom >= 3.5) {
+            message = "Aprobado"
+        } else {
+            message = "Reprobado"
         }
 
-        miBundle.putString("Nombre",name)
-        miBundle.putString("Materia",subject)
-        miBundle.putDouble("Nota1",nota1)
-        miBundle.putDouble("Nota2",nota2)
-        miBundle.putDouble("Nota3",nota3)
-        miBundle.putDouble("Promedio",prom)
-        miBundle.putString("message1",message)
-        intent.putExtras(miBundle)
+        val intent = Intent(this, ActivityMensage::class.java)
+        val content: Bundle = Bundle()
+        content.putString("Nombre", name)
+        content.putString("Materia", subject)
+        content.putDouble("Promedio", formattedAverage)
+        content.putDouble("Nota1",nota1)
+        content.putDouble("Nota2",nota2)
+        content.putDouble("Nota3",nota3)
+        content.putString("message1",message)
+
+        intent.putExtras(content)
         startActivity(intent)
-
-
     }
 
     }
